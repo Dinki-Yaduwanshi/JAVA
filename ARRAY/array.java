c7d7ae7c3496c462b2d6c323379ac4d81369907d
Dinki Yaduwanshi <91734640+anonymousdaisy5@users.noreply.github.com>
2022-08-30 11:56:45 +0530

    Create array.java

---
+++ ARRAY/array.java
@@ -0,0 +1,15 @@
+package com.java.array;
+
+public class Array {
+	
+		public static void main(String[] args) {
+			int A[]=new int[5];
+			int B[]={2,4,5,6,7};
+			int sum=0;
+		for(int i=0;i<B.length;i++){
+		    sum=sum+B[i];
+		    
+		}
+		System.out.println(sum);
+			}
+		}
