38d4b057a8b0f2c32cc7b76d8a05ba7b6e038e35
Dinki Yaduwanshi <91734640+anonymousdaisy5@users.noreply.github.com>
2022-08-30 19:32:50 +0530

    Create Deletion.java

---
+++ ARRAY/Deletion.java
@@ -0,0 +1,25 @@
+public class Deletion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int A[]=new int[10];
+	      A[0]=5;
+	      A[1]=9;
+	      A[2]=6;
+	      A[3]=10;
+	      A[4]=12;
+	      A[5]=7;
+	      
+	      for(int i=5;i>=2;i--) {
+	    	  A[i+1]=A[i];
+	    	  
+	      }
+	    A[2]=15;
+	    
+	    for(int x:A) {
+	    	System.out.print(x);
+	    }
+
+	}
+
+}
