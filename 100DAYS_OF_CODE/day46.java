fc20c8081e4992b694f98e0b8e6c7659c5f2a706
Dinki Yaduwanshi <91734640+Dinki-Yaduwanshi@users.noreply.github.com>
2023-03-04 23:33:44 +0530

    Create day46.java

---
+++ 100DAYS_OF_CODE/day46.java
@@ -0,0 +1,40 @@
+Day 46 coding Statement : Write Program to find sum of elements in an array
+
+Description
+
+Get an array as the input from the user and find the sum of the elements.
+
+Input
+
+Enter the size of array
+
+3
+
+Enter the elements of array
+
+5 10 15
+
+Output
+
+30
+
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+		int a = sc.nextInt();
+		int[] arr= new int[a];
+		for(int i=0;i<a;i++){
+		    arr[i]=sc.nextInt();
+		}
+		int max=0;  
+	for(int j=0;j<arr.length;j++){
+	    max+=arr[j];
+	}
+	System.out.println(max);
+	
+	}
+}
