9fa7f93d315720eb3c378ad42393a71c245e103c
Dinki Yaduwanshi <91734640+Dinki-Yaduwanshi@users.noreply.github.com>
2023-06-16 10:40:53 +0530

    Create Pattern6.java

---
+++ Pattern/Pattern6.java
@@ -0,0 +1,31 @@
+   *
+  **
+ ***
+****
+ ***
+  **
+   *
+
+     public class MyClass {
+    public static void main(String args[]) {
+     for(int i=1;i<=4;i++){
+      for(int j=3;j>=i;j--){
+          System.out.print(" ");
+      }
+      for(int k=1;k<=i;k++){
+          System.out.print("*");
+      }
+     System.out.println();
+     
+    }
+    for(int o=1;o<=4;o++){
+      for(int a=1;a<=o;a++){
+          System.out.print(" ");
+      }
+      for(int b=3;b>=o;b--){
+          System.out.print("*");
+      }
+       System.out.println();
+  }
+}
+}
