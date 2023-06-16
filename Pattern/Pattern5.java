17e6b9b97533f99f4a7143d6310e632534470384
Dinki Yaduwanshi <91734640+Dinki-Yaduwanshi@users.noreply.github.com>
2023-06-16 10:24:14 +0530

    Create Pattern5.java

---
+++ Pattern/Pattern5.java
@@ -0,0 +1,21 @@
+class HelloWorld {
+    public static void main(String[] args) {
+    System.out.println("1.");
+		for(int i=1;i<=4;i++){
+		     for(int k=1;k<i;k++){
+		        System.out.print(" ");
+		    }
+		    
+		    for(int j=4;j>=i;j--){
+		        System.out.print("*");
+		    }
+		   
+		    System.out.println();
+		}
+    }
+}
+
+****
+ ***
+  **
+   *
